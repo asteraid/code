026857d9fa99bd17210c65da9212bceae8e705e5
Andrew Pashetnykh <andpash@gmail.com>
2015-03-28 02:58:36 +0400

    fix values for callflow

--- routes/data/items.js
+++ routes/data/items.js
@@ -1,9 +1,10 @@
 exports.list = function (req, res) {
     var db = new database(req, res);
     if(db.connect) {
-        var type = req.param('type'),
-            id = req.param('id'),
-			custom_type = req.param('custom_type') || '';
+        var type = req.param('type');
+        var id = req.param('id');
+        var custom_type = req.param('custom_type') || '';
+        var id_class = req.param('id_class');
 			
             if(id == undefined) {
                 if( custom_type != '' )
@@ -11,8 +12,12 @@ exports.list = function (req, res) {
                     
                 if(type == 'context')
                     var query = "SELECT DISTINCT `id`, `type`, `name`, `metric`, `pattern`, `comment`, `readonly`, `visible` FROM vItems WHERE `type` = '" + type + "' AND `visible` = 1";
-                else
-					var query = "SELECT DISTINCT `id`, `type`, `name`, `metric`, `pattern`, `comment`, `custom_type`, if(`commented` = 0, 'No', 'Yes') `commented` FROM vItems WHERE `type` = '" + type + "' " + custom_type;
+                else {
+                    if (type == 'callflow')
+                        var query = "SELECT DISTINCT `id`, `type`, `name`, `metric`, `pattern`, `comment`, `custom_type`, if(`commented` = 0, 'No', 'Yes') `commented` FROM vItems WHERE `id_class` = " + id_class + " AND `type` = '" + type + "' " + custom_type;
+                    else
+                        var query = "SELECT DISTINCT `id`, `type`, `name`, `metric`, `pattern`, `comment`, `custom_type`, if(`commented` = 0, 'No', 'Yes') `commented` FROM vItems WHERE `type` = '" + type + "' " + custom_type;
+                }
             } else
                 var query = "SELECT DISTINCT * FROM vItems WHERE parent_id = " + id + " AND `type` IS NOT NULL";
                 // //var query = "SELECT A.id id, A.type type, A.name name, A.metric metric, A.pattern pattern, A.comment comment, B.order `order` FROM vItems A LEFT OUTER JOIN config_relations B ON (A.id = B.item_id) WHERE B.parent_id = " + id + " ORDER BY B.`order` ASC";
