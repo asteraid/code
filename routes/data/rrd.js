82b328dec9a3afcda148fb0c1c05670a312e8582
Andrew Pashetnykh <andpash@gmail.com>
2015-04-01 03:35:15 +0400

    dashboard for many nodes

--- routes/data/rrd.js
+++ routes/data/rrd.js
@@ -1,4 +1,5 @@
 var RRDTool		= require('node-rrdtool');
+var path      = '/home/andrew/git/ps-projects/test/rrd/hosts/';
 
 exports.list = function(req, res) {
 var db = new database(req, res);
@@ -35,11 +36,9 @@ var db = new database(req, res);
 }
 
 
-exports.get_data_interval = function(req, res) {
+/*exports.get_data_interval = function(req, res) {
 	var rrd			= new RRDTool();
 	var param		= req.param('param');
-	
-	//console.log(rrd, param);
 	
 	if (param) {
 		getRRDFetch(rrd, config.rrd_path, '-1440min', '-1min', '60', function(response) {
@@ -63,10 +62,159 @@ exports.get_data_interval = function(req, res) {
 	} else {
 		res.json({success: false});
 	}
+}*/
+
+exports.get_data_interval = function(req, res) {
+  var fs      = require('fs');
+	var rrd			= new RRDTool();
+	var param		= req.param('param');
+  var files   = fs.readdirSync(path);
+  var result  = {};
+  var time    = {};
+    time.start = '-1440min';
+    time.end   = '-1min';
+    time.step  = '60';
+  
+  var callbackFunc = function(response) {
+    var fileName = files.shift().split('.').slice(0, -1);
+
+    if (param && response !== undefined) {
+      result[fileName] = {};
+      result[fileName][param] = [];
+      
+      var paramIndex = response.headers.indexOf(param);
+      
+      response.data.forEach(function(item) {
+        if (isNaN(item[paramIndex]))
+          item[paramIndex] = 0;
+          
+        result[fileName][param].push([item[0]*1000, item[paramIndex]]);
+      });
+      
+      if (files.length > 0) {
+        getRRDFetch(rrd, path + files[0], time.start, time.end, time.step, callbackFunc);
+      } else {
+        res.json({success: true, result: result});
+      }
+		}
+  };
+  
+  getRRDFetch(rrd, path + files[0], time.start, time.end, time.step, callbackFunc);
+	
+  /*
+	if (param) {
+		getRRDFetch(rrd, config.rrd_path, '-1440min', '-1min', '60', function(response) {
+			var result = {};
+			result[param] = [];
+			
+			if (response !== undefined) {
+				var paramIndex = response.headers.indexOf(param);
+			
+				response.data.forEach(function(item) {
+					if (isNaN(item[paramIndex]))
+						item[paramIndex] = 0;
+						
+					result[param].push([item[0]*1000, item[paramIndex]]);
+				});
+			}
+				
+			res.json({success: true, result: result});
+		});
+	} else {
+		res.json({success: false});
+	}
+  */
 }
 
 exports.data_item = function (req, res) {
-	var rrd			= new RRDTool();
+  var fs      = require('fs');
+  var files   = fs.readdirSync(path);
+	var rrd     = new RRDTool();
+  var params  = req.param('params');
+  
+  var result = {};
+  
+  /*files.forEach(function(item) {
+    result[item.split('.').shift()] = {};
+  });*/
+  
+  //console.log(result);
+  //res.json({success: false});
+  
+  var callbackFunc = function(response) {
+    var fileName = files.shift().split('.').slice(0, -1);
+
+    if (params && response) {
+      result[fileName] = {};
+			params.forEach(function(item) {
+				result[fileName][item] = response[item].last_ds;
+			});
+      
+      if (files.length > 0) {
+        getRRDInfo(rrd, path + files[0], callbackFunc);
+      } else {
+        var count = Object.keys(result).length;
+        
+        // sum every param
+        var obj = {};
+        for (var index in result) { 
+          if (result.hasOwnProperty(index)) {
+            var attr = result[index];
+            
+            for (var prop in result[index]) {
+              if (result[index].hasOwnProperty(prop)) {
+                if (obj.hasOwnProperty(prop))
+                  obj[prop] = 0;
+
+                obj[prop] += 1 * result[index][prop];
+              }
+            }
+          }
+        }
+        
+        //average value
+        for (var i in obj) {
+          if (obj.hasOwnProperty(i)) {
+            obj[i] = obj[i]/count;
+          }
+        }
+        
+        res.json({success: true, result: obj});
+      }
+		}
+  };
+  
+  getRRDInfo(rrd, path + files[0], callbackFunc);
+  
+  /*getRRDInfo(rrd, '/home/andrew/git/ps-projects/test/rrd/hosts/' + files[0], function(response) {
+    var fileName = files.shift().split('.').slice(0, -1);
+    
+    if (params && response) {
+      result[fileName] = {};
+			params.forEach(function(item) {
+				result[fileName][item] = response[item].last_ds;
+			});
+      
+      if (files.length > 0) {}
+		}*/ //else {
+      //res.json({success: true, result: result});
+    //}
+    
+    //console.log(result);
+    
+    //res.json({success: false});
+		/*if (params && response) {
+			params.forEach(function(item) {
+				result[item] = response[item].last_ds;
+			});
+		} else {
+      res.json({success: true, result: result});
+    }
+		res.json({success: true, result: result});*/
+  //});
+  
+  
+  /*
 	var type		= req.param('type');
 	var params		= req.param('params');
 	
@@ -78,7 +226,7 @@ exports.data_item = function (req, res) {
 			});
 		
 		res.json({success: true, result: result});
-	});
+	});*/
 };
 
 function getRRDInfo(rrdtool, file, callback) {
