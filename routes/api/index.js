3361f671e6c9d80d40bf99c84f94fdbd32e71a0a
kain_pl <pushkar.valeriy@gmail.com>
2015-08-18 00:05:50 +0300

    перечень модулей API по /api/

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,18 @@
 
 exports.index = function (req, res) {
 
+    var fs = require('fs'),
+        path = require('path'),
+        baseDir = path.dirname(require.main.filename),
+        modules = [];
+
+    fs.readdirSync(path.join(baseDir, '/routes/api')).forEach(function(file) {
+        var fileNameParts = file.split('.');
+        if (fileNameParts[0] === 'index')
+            return;
+        modules.push({module: fileNameParts[0]});
+    });
+
+    res.json({modules: modules});
+
 };
