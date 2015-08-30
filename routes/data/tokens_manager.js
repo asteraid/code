bad50779036a3dc6d39610f075dd04f43c8d41ce
kain_pl <pushkar.valeriy@gmail.com>
2015-08-31 02:54:26 +0300

    Работа с токенами. Автоинициация.

--- routes/data/tokens_manager.js
+++ routes/data/tokens_manager.js
@@ -25,22 +25,40 @@ exports.listAllTokens = function(req, res) {
                     res.json({success: false, rows: [], message: 'The are no tokens registered!'});
             });
         else
-            db.query(req, 'CREATE TABLE a_conf.user_tokens (id int(11) UNSIGNED NOT NULL AUTO_INCREMENT, ' +
+            db.query(req, 'CREATE TABLE user_tokens (id int(11) UNSIGNED NOT NULL AUTO_INCREMENT, ' +
                 'user_id int(11) NOT NULL,  token varchar(36) NOT NULL,  begin_date datetime NOT NULL, ' +
                 'end_date datetime DEFAULT NULL,  active tinyint(1) NOT NULL,  comment text DEFAULT NULL, ' +
                 'PRIMARY KEY (id))  ENGINE = INNODB  AUTO_INCREMENT = 1  CHARACTER SET utf8  COLLATE utf8_general_ci;', function(err, results) {
 
                 if(!err)
-                    db.query(req, sQuery, function(err, results) {
-                        if (err) {
-                            res.json({success: false, rows: [], message: err.message });
-                            return;
-                        }
+                    db.query(req, 'CREATE PROCEDURE get_userIdByToken(IN token VARCHAR(36), OUT result TINYINT, OUT userId INT) ' +
+                        'SQL SECURITY INVOKER BEGIN DECLARE EXIT HANDLER FOR SQLEXCEPTION SET result = FALSE; ' +
+                        'SELECT user_id FROM user_tokens WHERE user_tokens.token = token INTO userId; ' +
+                        'IF userId > 0 THEN SET result = TRUE; ELSE SET result = FALSE; END IF; END', function(err, results) {
 
-                        if (results.length)
-                            res.json({success: true, rows: results, message: ''});
+                        if(!err)
+                            db.query(req, 'CREATE VIEW vUserTokens AS SELECT `ut`.`id` AS `id`, `ut`.`user_id` AS `user_id`, ' +
+                                '`u`.`login` AS `login`, `ut`.`token` AS `token`, `ut`.`begin_date` AS `begin_date`, ' +
+                                '`ut`.`end_date` AS `end_date`, `ut`.`active` AS `active`, `ut`.`comment` AS `comment` ' +
+                                'FROM (`user_tokens` `ut` JOIN `users` `u` ON ((`ut`.`user_id` = `u`.`id`)));', function(err, results) {
+
+                                if(!err)
+                                    db.query(req, sQuery, function(err, results) {
+                                        if (err) {
+                                            res.json({success: false, rows: [], message: err.message });
+                                            return;
+                                        }
+
+                                        if (results.length)
+                                            res.json({success: true, rows: results, message: ''});
+                                        else
+                                            res.json({success: false, rows: [], message: 'The are no tokens registered!'});
+                                    });
+                                else
+                                    res.json({success: false, message: err.message});
+                            });
                         else
-                            res.json({success: false, rows: [], message: 'The are no tokens registered!'});
+                            res.json({success: false, message: err.message});
                     });
                 else
                     res.json({success: false, message: err.message});
